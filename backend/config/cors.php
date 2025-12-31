078dd2dceef5ef69c8223f19c6e7b751482633a6
YERRABOTHU ADITHYA <yerrabothuadithya183@gmail.com>
2025-12-31 13:58:34 +0530

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://beyond-chats-assignment-kappa.vercel.app',
+        'https://beyondchats-assignment-igd3.onrender.com',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
